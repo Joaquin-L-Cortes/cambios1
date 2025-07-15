99fa878369ab1efdbb7673043cff80fd27327d6c
Joaquín Leonardo Cortes Camelo <219436754+Joaquin-L-Cortes@users.noreply.github.com>
2025-07-15 10:23:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ const metodosInvestigacion = [
 
 // Datos de las sociologías temáticas
 const sociologiasTematicas = [
-    { nombre: "Cultura y Sociedad en América Latina", creditos: 3},
+    { nombre: "Cultura y Sociedad en América Latina", creditos: 3, prerreq: [] },
     { nombre: "Riesgo, Experticia y Confianza", creditos: 3, prerreq: [] },
     { nombre: "Análisis Histórico y Sociológico del Municipio Colombiano", creditos: 3, prerreq: [] },
     { nombre: "Dominación Sexual y Racial", creditos: 3, prerreq: [] },
